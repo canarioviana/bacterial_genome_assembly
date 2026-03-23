7938210aea0935815b832936d95dd3452fea6919
Marcus V. C. Viana <131043224+canarioviana@users.noreply.github.com>
2026-03-23 14:48:30 -0300

    Update bga_software_installation.sh

--- bga_software_installation.sh
+++ bga_software_installation.sh
@@ -5,7 +5,7 @@
 # This file uses the .sh extension only to enable Bash syntax highlighting in text editors.
 #
 # Author: Marcus Vinicius Canário Viana
-# Date: 21/03/2026
+# Date: 23/03/2026
 # Repository: https://github.com/canarioviana/bacterial_genome_assembly
 # More info: see README.md in the repository
 
